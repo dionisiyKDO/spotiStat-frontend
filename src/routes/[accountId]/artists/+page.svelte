ebf44aa1cbff67590e26573643a259f96d726dfc
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-06-09 08:00:01 +0300

    reorganize artist stats

--- src/routes/[accountId]/artists/+page.svelte
+++ src/routes/[accountId]/artists/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
+    import D3Chart from "$lib/artist/TimelineChart.svelte";
     import {
         fetchArtistStats,
         fetchPlayedTracks,
         fetchTracks,
         type Track,
     } from "./load";
-    import D3Chart from "./D3Chart.svelte";
-    // import TrackList from "$lib/TrackList.svelte";
 
     let artist = $state("塞壬唱片-MSR");
     let selectedDate = $state("1976-01-01");
@@ -47,99 +46,148 @@
         fetchPlayedTracks(100, selectedDate, artist)
     );
     let tracksReq = $derived(fetchTracks());
+
 </script>
 
-<h1 class="text-3xl font-semibold">Artist stats</h1>
 
-<!-- Input -->
-{#await tracksReq}
-    <p>Loading.....</p>
-{:then tracks}
-    <div class="relative my-6">
-        <input
-            class="w-96 border p-2"
-            type="text"
-            placeholder="Search artist..."
-            bind:value={searchInput}
-            onfocus={() => (showSuggestions = true)}
-            onblur={() =>
-                setTimeout(() => {
-                    showSuggestions = false;
-                }, 200)}
-            oninput={() => filterTracks(tracks)}
-        />
 
-        {#if showSuggestions && filteredTracks.length > 0}
-            <ul class="absolute bg-black border mt-1 w-96">
-                {#each filteredTracks as track}
-                    <li class="p-2 cursor-pointer hover:bg-gray-800">
-                        <button
-                            type="button"
-                            class="w-full text-left"
-                            onclick={() => {
-                                showSuggestions = false;
-                                searchInput = track.artist;
-                                artist = track.artist;
-                            }}
-                        >
-                            {track.artist}
-                        </button>
-                    </li>
-                {/each}
-            </ul>
-        {/if}
-    </div>
-{/await}
+<section class="track-stats-page">
+  <!-- Track Search / Selector -->
+    {#await tracksReq}
+        <p>Loading.....</p>
+    {:then tracks}
+        <header class="track-selector">
+            <h1 class="text-3xl font-semibold mb-2 inline-block">Artist stats</h1>
+            <div class="relative mb-4">
+                <input
+                    class="w-96 border p-2"
+                    type="text"
+                    placeholder="Search artist..."
+                    bind:value={searchInput}
+                    onfocus={() => (showSuggestions = true)}
+                    onblur={() =>
+                        setTimeout(() => {
+                            showSuggestions = false;
+                        }, 200)}
+                    oninput={() => filterTracks(tracks)}
+                />
 
-{#await artistStatsReq}
-    <p>Loading.....</p>
-{:then artistStats}
-    <div>
-        <h1>{artist}</h1>
-    </div>
-
-    <div>
-        {#if artistStats.timeline_data.length > 0}
-            <D3Chart
-                selectedDate={(value) => {
-                    selectedDate = value;
-                }}
-                timeline_data={artistStats.timeline_data}
-            />
-        {/if}
-
-        <p>Total plays: {artistStats.total_plays}</p>
-        <p>
-            Total time played: {(artistStats.total_ms_played / 3600000).toFixed(
-                2
-            )} hours
-        </p>
-        <p>
-            First played: {new Date(
-                artistStats.first_played
-            ).toLocaleDateString()}
-        </p>
-        <p>
-            Last played: {new Date(
-                artistStats.last_played
-            ).toLocaleDateString()}
-        </p>
-    </div>
-{/await}
-
-{#await playedTracksReq}
-    <p>Loading.....</p>
-{:then tracks}
-    <div class="m-8">
-        {#if tracks.length > 0}
-            <div class="flex gap-2 mb-2">
-                <h2 class="py-1 text-3xl font-semibold">
-                    Tracks from: {selectedDate}
-                </h2>
+                {#if showSuggestions && filteredTracks.length > 0}
+                    <ul class="absolute bg-black border mt-1 w-9/12">
+                        {#each filteredTracks as track}
+                        <li class="p-2 cursor-pointer hover:bg-gray-800">
+                            <button
+                                type="button"
+                                class="w-full text-left"
+                                onclick={() => {
+                                    showSuggestions = false;
+                                    searchInput = track.artist;
+                                    artist = track.artist;
+                                }}
+                            >
+                                {track.artist}
+                            </button>
+                        </li>
+                        {/each}
+                    </ul>
+                {/if}
             </div>
-            <div class="mt-4">
-                <!-- <TrackList {tracks} /> -->
+        </header>
+    {/await}
+
+    {#await artistStatsReq}
+        <p>Loading.....</p>
+    {:then artistStats}
+        <section class="grid grid-cols-2 md:grid-cols-2 gap-6">
+            <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+                <h2 class="text-sm text-(--secondary-text) mb-1">Total Plays</h2>
+                <p class="text-xl font-semibold mt-1">{artistStats.total_plays}</p>
             </div>
-        {/if}
-    </div>
-{/await}
+            <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+                <h2 class="text-sm text-(--secondary-text) mb-1">Total Time Played</h2>
+                <p class="text-xl font-semibold mt-1">
+                    {#if (artistStats.total_ms_played / 3600000) > 3}
+                        {(artistStats.total_ms_played / 3600000).toFixed(2)} hours
+                    {:else}
+                        {(artistStats.total_ms_played / 60000).toFixed(2)} minutes
+                    {/if}
+                </p>
+            </div>
+            <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+                <h2 class="text-sm text-(--secondary-text) mb-1">First Play</h2>
+                <p class="text-xl font-semibold mt-1">{new Date(artistStats.first_played).toLocaleDateString()}</p>
+            </div>
+            <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+                <h2 class="text-sm text-(--secondary-text) mb-1">Last (recorded) Play</h2>
+                <p class="text-xl font-semibold mt-1">{new Date(artistStats.last_played).toLocaleDateString()}</p>
+            </div>
+            <!-- Optional Add-ons -->
+            <!-- <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+                <h2 class="text-sm text-(--secondary-text) mb-1">Longest streak</h2>
+                <p class="text-xl font-semibold mt-1">7 days in a row</p>
+            </div>
+            <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+                <h2 class="text-sm text-(--secondary-text) mb-1">First 30 days trend (Was it an instant favorite or a grower?)</h2>
+                <p class="text-xl font-semibold mt-1">aboba Jan 15, 2024</p>
+            </div> -->
+
+            <!-- Artist meta maybe (spotify api required) -->
+            <!-- {#await trackMetaReq}
+                <p class="loading">Loading track meta info from Spotify...</p>
+            {:then track_meta}
+                <div class="flex flex-col">
+                    meta
+                    <div>
+                        Artists:
+                        {#if track_meta.artists.length === 0}
+                            <p>No artists found</p>
+                        {:else}
+                            {#each track_meta.artists as artist, index}
+                                {artist.name}{index < track_meta.artists.length - 1 ? ' / ' : ''}
+                            {/each}
+                        {/if}
+                    </div>
+
+                    <div>
+                        <p>Album name: {track_meta.album.name}</p>
+                        <p>Album release date: {track_meta.album.release_date}</p>
+                        <p>Track popularity: {track_meta.popularity}</p>
+                    </div>
+                </div>
+            {/await} -->
+        </section>
+
+        <!-- Chart Section -->
+        <section class="track-chart mt-10">
+            <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
+            <h2 class="text-sm text-(--secondary-text) mb-1 ml-4">Minutes of artist played per day</h2>
+                {#if artistStats.timeline_data.length > 0}
+                    <D3Chart
+                        selectedDate={(value) => {
+                            selectedDate = value;
+                        }}
+                        timeline_data={artistStats.timeline_data}
+                    />
+                {/if}
+            </div>
+        </section>
+    {/await}
+    
+    <!-- {#await playedTracksReq}
+        <p>Loading.....</p>
+    {:then tracks}
+        <div class="m-8">
+            {#if tracks.length > 0}
+                <div class="flex gap-2 mb-2">
+                    <h2 class="py-1 text-3xl font-semibold">
+                        Tracks from: {selectedDate}
+                    </h2>
+                </div>
+                <div class="mt-4">
+                    <TrackList {tracks} /> 
+                </div>
+            {/if}
+        </div>
+    {/await} -->
+
+</section>
