ba4494f00bf0cf281bbee18c49680ad5824a22ed
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-06-13 20:19:28 +0300

    adapt page for unionized fetch

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -2,16 +2,7 @@
 	import type { PageProps } from './$types';
     import BarChart from "$lib/dashboard/BarChart.svelte";
     import TimelineChart from "$lib/dashboard/TimelineChart.svelte";
-    import {
-        fetchTotalListeningTime,
-        fetchPlatformStats,
-        fetchSkipStats,
-        fetchUniqueTracksCount,
-        fetchLongestListeningSession,
-        fetchDailyTrends,
-        fetchHourlyTrends,
-        fetchWeeklyTrends,
-    } from "./load";
+    import { fetchAllStats } from "./load";
 
     let { data }: PageProps = $props();
     const username = data.username
@@ -28,46 +19,49 @@
     ];
 
 
+    async function reqUploadHistory() {
+        try {
+            const response = await fetch(`/api/db/upload_history?username=${username}`);
+            if (!response.ok) {
+                const data = await response.json();
+                const error = data.error || "Failed to fetch total listening time";
+                console.log(error);
+                return null;
+            }
+
+            const data = (await response.json());
+            console.log(data);
+            
+            return data;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
     
-    const summaryPromise = Promise.all([
-        fetchTotalListeningTime(username),
-        fetchUniqueTracksCount(username),
-        fetchLongestListeningSession(username),
-    ]);
-    const trendsPromise = Promise.all([
-        fetchHourlyTrends(username),
-        fetchWeeklyTrends(username),
-        fetchDailyTrends(username),
-    ]);
-    const behaviorPromise = Promise.all([
-        fetchPlatformStats(username),
-        fetchSkipStats(username),
-    ]);
-    // let mostSkippedTracksReq = fetchMostSkippedTracks(username);
-    // let endReasonsReq = fetchEndReasons(username);
+    let StatsReq = fetchAllStats(username);
 
-    async function reqUploadHistory() {
-    try {
-        const response = await fetch(`/api/db/upload_history?username=${username}`);
-        if (!response.ok) {
-            const data = await response.json();
-            const error = data.error || "Failed to fetch total listening time";
-            console.log(error);
-            return null;
-        }
-
-        const data = (await response.json());
-        console.log(data);
-        
-        return data;
-    } catch (err) {
-        console.log(err);
-        return null;
-    }
-}
+    const summaryPromise    = StatsReq.then(data => ({
+        tltData: data.total_listening_time, 
+        uniqData: data.unique_tracks_count, 
+        sessData: data.longest_session,
+        topTracks: data.top_tracks,
+        topArtists: data.top_artists,
+    }));
+    const trendsPromise     = StatsReq.then(data => ({
+        hourlyTrends: data.listening_by_hour, 
+        weeklyTrends: data.listening_by_weekday, 
+        monthsTrends: data.listening_by_month, 
+        yearlyTrends: data.listening_by_year, 
+        dailyTrends: data.listening_by_date,
+    }));
+    const behaviorPromise   = StatsReq.then(data => ({
+        platfData: data.platform_stats, 
+        skipStats: data.skip_stats,
+        }));
 </script>
 
 <div>
@@ -86,22 +80,13 @@
                     <a class="link" href={link}>{name}</a>
                 {/each}
             </div>
-            <!-- <footer class="pt-8 border-t mt-12">
-                <h3 class="text-lg font-semibold mb-4">Explore Detailed Stats</h3>
-                <div class="flex flex-wrap gap-4">
-                <a href="/dashboard/track" class="bg-blue-100 text-blue-800 px-4 py-2 rounded hover:bg-blue-200">Track Stats</a>
-                <a href="/dashboard/artist" class="bg-green-100 text-green-800 px-4 py-2 rounded hover:bg-green-200">Artist Stats</a>
-                <a href="/dashboard/trends" class="bg-purple-100 text-purple-800 px-4 py-2 rounded hover:bg-purple-200">Trends</a>
-                <a href="/dashboard/compare" class="bg-gray-800 text-gray-800 px-4 py-2 rounded hover:bg-gray-200">Compare</a>
-                </div>
-            </footer> -->
         </header>
         <hr class="mt-4">
 
         <!-- Summary Cards -->
         {#await summaryPromise}
             <p class="loading">Loading Total Listening Time...</p>
-        {:then [tltData, uniqData, sessData]}
+        {:then {tltData, uniqData, sessData, topTracks, topArtists}}
             <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                 <div class="bg-(--surface) shadow rounded-xl p-4">
                     <h2 class="text-sm text-(--secondary-text)">Total Listening Time</h2>
@@ -110,18 +95,13 @@
                         <p class="mt-1"> or </p> 
                         <p class="font-semibold mt-1">{numberWithCommas(tltData.total_listening_hours.toFixed(0))}h</p>
                     </div>
-                    <!-- <ul class="grid grid-cols-2">
-                        <li class="text-2xl font-semibold mt-1">{numberWithCommas(tltData.total_listening_days.toFixed(0))}d</li>
-                        <li class="text-2xl font-semibold mt-1">{numberWithCommas(tltData.total_listening_hours.toFixed(0))}h</li>
-                        <li class="text-2xl font-semibold mt-1">{numberWithCommas(tltData.total_listening_minutes.toFixed(0))}m</li>
-                    </ul> -->
                 </div>
                 <div class="bg-(--surface) shadow rounded-xl p-4">
                     <h2 class="text-sm text-(--secondary-text)">Unique Tracks</h2>
                     <p class="text-2xl font-semibold mt-1">{numberWithCommas(uniqData.unique_tracks_count)}</p>
                 </div>
 
-                <!-- TODO: Make tio artist and top tracks list 5 elemts instead of one name -->
+                <!-- TODO: Make top artist and top tracks list 5 elemts instead of one name -->
                 <div class="bg-(--surface) shadow rounded-xl p-4">
                     <h2 class="text-sm text-(--secondary-text)">Top Artist</h2>
                     <p class="text-2xl font-semibold mt-1">[Porter]</p>
@@ -142,7 +122,7 @@
         <!-- Trends Area -->
         {#await trendsPromise}
             <p class="loading">Loading Hourly Trends...</p>
-        {:then [hourlyTrends, weeklyTrends, dailyTrends]}
+        {:then {hourlyTrends, weeklyTrends, dailyTrends, yearlyTrends, monthsTrends}}
             
             <section class="grid grid-cols-1 lg:grid-cols-2 gap-6">
 
@@ -150,15 +130,23 @@
                 <div class="bg-(--surface) shadow rounded-xl p-4 flex flex-col col-span-2">
                 <!-- <div class="bg-(--surface) shadow rounded-xl p-4 flex flex-col"> -->
                     <h3 class="text-lg font-semibold mb-2">Timeline of Activity</h3>
-                    <TimelineChart data={dailyTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'days'} />
+                    <TimelineChart data={dailyTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'date'} />
                 </div>
                 <div class="bg-(--surface) shadow rounded-xl p-4">
                     <h3 class="text-lg font-semibold mb-2">Hourly Listening Pattern</h3>
                     <BarChart data={hourlyTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'hour'} />
                 </div>
                 <div class="bg-(--surface) shadow rounded-xl p-4">
-                    <h3 class="text-lg font-semibold mb-2">Day-of-Week Listening</h3>
-                    <BarChart data={weeklyTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'day_of_week'} />
+                    <h3 class="text-lg font-semibold mb-2">Day-of-Week Listening Pattern</h3>
+                    <BarChart data={weeklyTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'weekday'} />
+                </div>
+                <div class="bg-(--surface) shadow rounded-xl p-4">
+                    <h3 class="text-lg font-semibold mb-2">Yearly Listening Pattern</h3>
+                    <BarChart data={yearlyTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'year'} />
+                </div>
+                <div class="bg-(--surface) shadow rounded-xl p-4">
+                    <h3 class="text-lg font-semibold mb-2">Monthly Listening Pattern</h3>
+                    <TimelineChart data={monthsTrends} yAxisLabel={'total_ms_played'} xAxisLabel={'month'} />
                 </div>
             </section>
         {/await}
@@ -166,7 +154,7 @@
         <!-- Behavior Insights -->
         {#await behaviorPromise}
             <p class="loading">Loading Total Platform stats...</p>
-        {:then [platfData, skipStats]}
+        {:then {platfData, skipStats}}
             <section class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div class="bg-(--surface) shadow rounded-xl p-4 gap-4">
                     <h2 class="text-sm text-(--secondary-text) mb-1">Platform Usage</h2>
