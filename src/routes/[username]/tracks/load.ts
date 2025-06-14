02216fad8009b4a59bf1d4acc95ee2d697f1ffe8
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-06-14 17:06:54 +0300

    refactor load.ts for track stats

--- src/routes/[username]/tracks/load.ts
+++ src/routes/[username]/tracks/load.ts
@@ -1,100 +1,40 @@
 import { tick } from "svelte";
 
-export interface TrackStats {
-    avg_playtime_per_play: number;
-    distinct_days_played: number;
-    first_played: string;
-    last_played: string;
-    most_frequent_play_count: number;
-    most_frequent_play_hour: number;
-    timeline_data: TimelineData[];
-    total_ms_played: number;
-    total_plays: number;
-    track_id: string;
-}
 
-export interface TrackMeta {
-    album: Album;
-    artists: Artist[];
-    available_markets: string[];
-    disc_number: number;
-    duration_ms: number;
-    explicit: boolean;
-    external_ids: ExternalIds;
-    external_urls: ExternalUrls;
-    href: string;
-    id: string;
-    is_local: boolean;
-    name: string;
-    popularity: number;
-    preview_url: string;
-    track_number: number;
-    type: string;
-    uri: string;
-}
 
-export interface Tracks {
+export interface TopTrack {
     artist: string;
     play_count: number;
     spotify_track_uri: string;
     total_ms_played: number;
+    total_hours: number;
     track_name: string;
 }
 
-interface TimelineData {
-    date: string | Date;
-    play_count: number;
-    total_ms_played: number;
-    total_minutes_played?: number;
+export interface TrackStats {
+  avg_playtime_per_play: number
+  distinct_days_played: number
+  first_played: string
+  last_played: string
+  song_length: number
+  timeline_data: TimelineData[]
+  total_ms_played: number
+  total_plays: number
+  track_id: string
 }
 
-interface Album {
-    album_type: string;
-    artists: Artist[];
-    available_markets: string[];
-    external_urls: ExternalUrls;
-    href: string;
-    id: string;
-    images: Image[];
-    name: string;
-    release_date: string;
-    release_date_precision: string;
-    total_tracks: number;
-    type: string;
-    uri: string;
+export interface TimelineData {
+  date: string
+  play_count: number
+  total_ms_played: number
 }
 
-interface Artist {
-    external_urls: ExternalUrls;
-    href: string;
-    id: string;
-    name: string;
-    type: string;
-    uri: string;
-}
 
-interface ExternalUrls {
-    spotify: string;
-}
 
-interface Image {
-    height: number;
-    url: string;
-    width: number;
-}
-
-interface ExternalIds {
-    isrc: string;
-}
-
-export async function fetchTracks(
-    username,
-    limit_count: number = 5,
-    limit_play: number = 1000
-): Promise<Tracks[] | null> {
+export async function fetchTracks(username: string): Promise<TopTrack[] | null> {
     try {
         const response = await fetch(
-            `/api/db/history/played-tracks?username=${username}&limit_count=${limit_count}&limit_play=${limit_play}&group_by=artist,track`
+            `/api/db/stats/${username}/listened-tracks`
         );
 
         if (!response.ok) {
@@ -104,20 +44,17 @@ export async function fetchTracks(
             return null;
         }
 
-        const data = (await response.json()) as { played_tracks: Tracks[] };
-        return data.played_tracks;
+        const data: TopTrack[] = await response.json();
+        return data;
     } catch (err) {
         console.log(err);
         return null;
     }
 }
 
-export async function fetchTrackStats(
-    username,
-    track_id: string
-): Promise<TrackStats | null> {
+export async function fetchTrackStats(username: string, track_id: string): Promise<TrackStats | null> {
     try {
-        const response = await fetch(`/api/db/history/track/${track_id}/stats?username=${username}`);
+        const response = await fetch(`/api/db/stats/${username}/track/${track_id}/stats`);
 
         if (!response.ok) {
             const data = await response.json();
@@ -128,6 +65,8 @@ export async function fetchTrackStats(
 
         const data = (await response.json()) as TrackStats;
         data.timeline_data = fillMissingDates(data);
+        console.log(data);
+        
         return data;
     } catch (err) {
         console.log(err);
@@ -135,28 +74,29 @@ export async function fetchTrackStats(
     }
 }
 
-// TODO: fix spotify api call
-export async function fetchTrackMeta(
-    track_id: string
-): Promise<TrackMeta | null> {
-    try {
-        const response = await fetch(
-            `/api/spotify/track_meta?track_id=${track_id}`
-        );
-        const data = await response.json();
-        if (!response.ok) {
-            const err = await response.json();
-            console.log(err);
-            return null;
-        }
-        // await new Promise(r => setTimeout(r, 2000));
 
-        return data.track_meta as TrackMeta;
-    } catch (err) {
-        console.log(err);
-        return null;
-    }
-}
+
+// TODO: fix spotify api call
+// export async function fetchTrackMeta(
+//     track_id: string
+// ): Promise<TrackMeta | null> {
+//     try {
+//         const response = await fetch(
+//             `/api/spotify/track_meta?track_id=${track_id}`
+//         );
+//         const data = await response.json();
+//         if (!response.ok) {
+//             const err = await response.json();
+//             console.log(err);
+//             return null;
+//         }
+
+//         return data.track_meta as TrackMeta;
+//     } catch (err) {
+//         console.log(err);
+//         return null;
+//     }
+// }
 
 
 
